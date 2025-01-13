376ed033efaf99a389813d3557f824c7175bbf4f
LancerA <lancera.thn@gmail.com>
2025-01-13 10:36:55 +0800

    first attempt to fix img path

--- init.sh
+++
@@ -1,6 +0,0 @@
-git init
-git remote add origin https://github.com/codelancera-offical/My-Blog.git
-git add .
-git commit -m "Initial commit, and set github actions for deployment"
-git branch -M main
-git push -u origin main
